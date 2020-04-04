e684b4c664b69ef577fefa50be4c862b40bfa397
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-04-04 05:12:21 -0400

    aa

--- config/config.js
+++ config/config.js
@@ -17,10 +17,10 @@ module.exports = {
   },
   production: {
     use_env_variable: process.env.DATABASE_URL,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    // username: process.env.DB_USERNAME,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_NAME,
+    // host: process.env.DB_HOST,
     dialect: 'postgres',
     operatorsAliases: 0
   }
